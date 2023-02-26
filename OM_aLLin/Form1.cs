c811c8fee265bbc40c2c708228014da2609ceecd
ravshan <ravshannedonat@gmail.com>
2023-02-26 15:37:35 +0300

    initialization done

--- OM_aLLin/Form1.cs
+++ OM_aLLin/Form1.cs
@@ -16,5 +16,24 @@ namespace OM_aLLin
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            run_test(richTextBox1);
+        }
+
+        static void run_test(RichTextBox richTextBox)
+        {
+
+            int[,] priceMap = new int[,] {
+            { 2, 3, 4, 3 },
+            { 5, 3, 1, 2 },
+            { 2, 1, 4, 2 }
+            };
+            int[] storage = new int[] { 414, 138, 184 };
+            int[] needs = new int[] {276, 92, 138, 184 };
+            TrafficOptimization TO = new TrafficOptimization(priceMap, storage, needs);
+            richTextBox.Text = TO.GetInfo();
+        }
     }
 }
---
+++ OM_aLLin/TrafficOptimization.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OM_aLLin
+{
+    internal class TrafficOptimization
+    {
+        int[,] priceMap;
+        int[,] trafficMap;
+        int[] storage; // n, srtokes
+        int[] needs; // m, columns
+
+        // для n строки
+        //Console.WriteLine(arr.GetLength(0));
+        // для m столбцы
+        //Console.WriteLine(arr.GetLength(1));
+
+        public TrafficOptimization()
+        {
+
+        }
+
+
+        // initialize rightly the whole quest
+        public TrafficOptimization(int[,] priceMap, int[] storage, int[] needs)
+        {
+            // check is the question is closed
+            // are needs and storage equel
+            int check = CheckClose(storage, needs);
+            int[,] tmpPriceMap;
+            int[] tmpStorage;
+            int[] tmpNeeds;
+            if (check == 0)
+            {
+                tmpStorage = storage;
+                tmpNeeds = needs;
+                //tmpPriceMap = priceMap;
+            }
+            else if(check < 0) // needs > storage, have to add storage
+            {
+                CopyPlusOne(storage, check,out tmpStorage);
+                tmpNeeds = needs;
+            }
+            else // storage > needs, have to add needs
+            {
+                CopyPlusOne(needs, check, out tmpNeeds);
+                tmpStorage = storage;
+            }
+
+            CreateMaps(priceMap, tmpStorage, tmpNeeds, out this.priceMap, out this.trafficMap);
+            this.storage = tmpStorage;
+            this.needs = tmpNeeds;
+        }
+
+        private void CreateMaps(int[,] priceMap, int[] storage, int[] needs, out int[,]tmpMap, out int[,] tmpTrafficMap)
+        {
+            // resizing to new sizes main arrays
+            tmpMap = new int[storage.Length,needs.Length];
+            tmpTrafficMap = new int[storage.Length, needs.Length];
+            // setting to zero all values
+            for(int i = 0; i < tmpMap.GetLength(0); i++)
+            {
+                for(int j = 0; j < tmpMap.GetLength(1); j++)
+                {
+                    tmpMap[i, j] = 0;
+                    tmpTrafficMap[i, j] = 0;
+                }
+            }
+
+            // copying from main price map, to out new
+            for(int i =0; i < priceMap.GetLength(0); i++)
+            {
+                for(int j = 0; j < priceMap.GetLength(1); j++)
+                {
+                    tmpMap[i, j] = priceMap[i, j];
+                }
+            }
+        }
+
+        private void CopyPlusOne(int[] from, int check,out int[] to)
+        {
+            to = new int[from.Length + 1];
+            for(int i = 0; i < from.Length; i++)
+            {
+                to[i] = from[i];
+            }
+            to[to.Length - 1] = Math.Abs(check);
+        }
+
+        private int CheckClose(int[] storage, int[] needs)
+        {
+            return storage.Sum() - needs.Sum();
+        }
+
+        public string GetInfo()
+        {
+            string result = "";
+            result += "Price info\n";
+            for(int i = 0; i < priceMap.GetLength(0); i++)
+            {
+                for(int j = 0; j < priceMap.GetLength(1); j++)
+                {
+                    result += $"{priceMap[i, j]}\t";
+                }
+                result += $"{this.storage[i]}\n";
+            }
+            for (int i = 0; i < needs.Length; i++)
+                result += $"{needs[i]}\t";
+            result += "\n";
+            return result;
+        }
+    }
+}
